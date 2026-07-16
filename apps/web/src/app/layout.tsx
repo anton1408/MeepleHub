abb5e10b4c2f74de265c6ea7da427cb4b4082fbc
luganskyi.anton <luganskyi.anton@gmail.com>
2026-07-16 14:41:45 +0300

    chore(web): add tailwindcss

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Header } from '@/widgets/header';
 
 export const metadata: Metadata = {
   title: "MeepleHub",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uk">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }

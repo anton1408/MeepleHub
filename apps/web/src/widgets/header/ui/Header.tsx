fa357c97f9bbe477d6f57ca69698838a14b18e1e
luganskyi.anton <luganskyi.anton@gmail.com>
2026-09-07 17:26:10 +0300

    feat(web): lay out header with search and auth buttons

--- apps/web/src/widgets/header/ui/Header.tsx
+++ apps/web/src/widgets/header/ui/Header.tsx
@@ -1,5 +1,7 @@
 import { cn } from '@meeplehub/ui-kit';
 import { Logo } from '@/shared/ui/Logo';
+import { UiInput } from '@meeplehub/ui-kit/ui/UiInput';
+import { UiButton } from '@meeplehub/ui-kit/ui/UiButton';
 
 type HeaderProps = {
   className?: string;
@@ -7,8 +9,13 @@ type HeaderProps = {
 
 export default function Header({ className }: HeaderProps) {
   return (
-    <div className={cn('bg-card h-18 w-full p-4', className)}>
+    <div className={cn('bg-card flex h-18 w-full items-center p-4', className)}>
       <Logo />
+      <UiInput border-radius="full" type="search" placeholder="Search..." className="ml-8 max-w-xs" />
+      <div className="ml-auto flex items-center gap-2">
+        <UiButton variant={'secondary'}>Login</UiButton>
+        <UiButton>Sign Up</UiButton>
+      </div>
     </div>
   );
 }
